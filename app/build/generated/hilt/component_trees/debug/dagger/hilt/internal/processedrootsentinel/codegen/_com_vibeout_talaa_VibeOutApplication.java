69f09b016841915dd9243ef81c7a4778a2685353
Karim Othman <karim.amsha@gmail.com>
2026-06-16 14:51:33 +0300

    Stop tracking generated Android build artifacts

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_vibeout_talaa_VibeOutApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.vibeout.talaa.VibeOutApplication"
-)
-public final class _com_vibeout_talaa_VibeOutApplication {
-}
--- app/build/intermediates/classes/debug/transformDebugClassesWithAsm/dirs/META-INF/proguard/com_vibeout_talaa_feature_notifications_NotificationsViewModel_HiltModules_KeyModule_LazyClassKeys.pro
+++
@@ -1,1 +0,0 @@
--keep,allowobfuscation,allowshrinking class com.vibeout.talaa.feature.notifications.NotificationsViewModel
